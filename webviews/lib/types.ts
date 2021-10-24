f8df50089c454a78e32294e2261e2f633af9b9b9
Tim <harvard90873@gmail.com>
2021-10-24 09:27:32 +0200

    update types

--- webviews/lib/types.ts
+++ webviews/lib/types.ts
@@ -2,9 +2,9 @@ export const X = "X";
 export const O = "O";
 export const EMPTY = null;
 export type Cell = typeof X | typeof O | typeof EMPTY;
-export type Row = [Cell, Cell, Cell];
+export type Row = Cell[];
 export type Action = [number, number];
-export type Board = [Row, Row, Row];
+export type Board = Row[];
 
 export interface MinimaxPacket {
   value: number;
