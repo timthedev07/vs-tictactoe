b9666d6eb1f277489e458dd0b9de9fdaf103485b
Tim <harvard90873@gmail.com>
2021-10-24 13:13:34 +0200

    update board

--- webviews/components/Board.svelte
+++ webviews/components/Board.svelte
@@ -1,24 +1,30 @@
 <script lang="ts">
   import { initialState, player } from "../lib/gameLogic";
   import { Board, Cell, EMPTY } from "../lib/types";
+  import CellComponent from "./Cell.svelte";
 
   let board: Board = initialState();
   let winner: Cell = EMPTY;
-  let cellComponents = [];
   $: currPlayer = player(board);
-  $: {
-    for (const row of board) {
-      for (const cell of row) {
-        cellComponents.push(cell);
-      }
-    }
-  }
 </script>
 
+<pre>Current player: {currPlayer}</pre>
+<pre>Winner player: {winner}</pre>
 <div class="board">
-  <pre>Current player: {currPlayer}</pre>
-  <pre>Winner player: {winner}</pre>
+  {#each board as row}
+    {#each row as cell}
+      <CellComponent {cell} />
+    {/each}
+  {/each}
 </div>
 
 <style>
+  .board {
+    --size: 150px;
+    height: var(--size);
+    width: var(--size);
+    display: grid;
+    grid-template-columns: auto auto auto;
+    grid-template-rows: auto auto auto;
+  }
 </style>
