38655f852e717d04b1d98dd8439f98336f7fe2df
Tim <harvard90873@gmail.com>
2021-10-24 13:13:20 +0200

    tweak cell

--- webviews/components/Cell.svelte
+++ webviews/components/Cell.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  import type { Cell } from "../lib/types";
+
+  export let cell: Cell;
+
+  let disabled: boolean;
+  $: disabled = cell === "O" || cell === "X";
 </script>
 
-<div class="svelte" />
+<button {disabled} class="cell">{cell}</button>
 
 <style>
+  .cell {
+    border: white solid 1px;
+    background-color: goldenrod;
+  }
 </style>
