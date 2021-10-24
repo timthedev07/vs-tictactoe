aca3d4450656e7096c6bcb6da888e3c007d5a698
Tim <harvard90873@gmail.com>
2021-10-24 12:14:26 +0200

    add board component

--- webviews/App.svelte
+++ webviews/App.svelte
@@ -1,6 +1,9 @@
+<script>
+  import Board from "./components/Board.svelte";
+</script>
+
 <main>
   <h1>Hello from Svelte</h1>
   <button>Click here to get rid of homework</button>
-  <input type="range" />
-  <input placeholder="Username" />
+  <Board />
 </main>
