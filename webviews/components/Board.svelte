e4fc1cdef610d38145b2ac0916d6cc12b36b5ada
Tim <harvard90873@gmail.com>
2021-10-24 09:23:10 +0200

    create new components

---
+++ webviews/components/Board.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { initialState } from "../lib/gameLogic";
+  import type { Board } from "../lib/types";
+  import Cell from "./Cell.svelte";
+
+  const board: Board = initialState();
+</script>
+
+<div class="board" />
+
+<style>
+</style>
